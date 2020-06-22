e02eaf566e85cca7456d3a5cf6b18809efafc5a1
silverstar194 <maloney4@wisc.edu>
2020-06-21 19:46:05 -0700

    changed log limit

--- client.py
+++ client.py
@@ -24,7 +24,7 @@ args = parser.parse_args()
 
 logger = logging.getLogger(__name__)
 
-file_handler = RotatingFileHandler('websocket_server.log', mode='a', maxBytes=5*1024*1024*1024, backupCount=2, encoding=None, delay=0)
+file_handler = RotatingFileHandler('websocket_server.log', mode='a', maxBytes=5*1024, backupCount=0, encoding=None, delay=0)
 
 handlers = [file_handler]
 
@@ -104,7 +104,7 @@ async def node_events():
 
                 while True:
                     result = (await websocket.recv())
-                    post_data = json.loads(result)
+                post_data = json.loads(result)
                     receive_time = post_data['message']['election_info']['time']
 
                     block_data = post_data['message']['block']
